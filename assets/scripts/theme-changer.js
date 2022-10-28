8bb6bdbedd5db1f21b4fb4739fc022d96354f643
Denis Solntsev <denis.solntsev@gmail.com>
2022-10-28 07:48:23 +0300

    Added theme-changer to main page

--- assets/scripts/theme-changer.js
+++ assets/scripts/theme-changer.js
@@ -1,6 +1,12 @@
 const changer = document.getElementById('theme-changer');
+const burger_changer = document.getElementById('burger-theme-changer');
 const html = document.documentElement;
 
 changer.addEventListener('change', function() {
     html.toggleAttribute('data-theme-dark');
 });
+
+burger_changer.addEventListener('change', function() {
+    html.toggleAttribute('data-theme-dark');
+});
+
