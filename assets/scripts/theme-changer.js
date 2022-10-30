797f81635c843b76589bc1b2b5570c2cde69e01d
Denis Solntsev <denis.solntsev@gmail.com>
2022-10-30 05:20:15 +0300

    current work

--- assets/scripts/theme-changer.js
+++ assets/scripts/theme-changer.js
@@ -2,16 +2,14 @@ const html = document.documentElement;
 const themeChangers = document.querySelectorAll('.theme_changer');
 const themes = document.querySelectorAll('[data-type="theme"]');
 
-function themeSwitch(otherSwitcherNum) {
-  themeChangers[otherSwitcherNum].checked = !themeChangers[otherSwitcherNum].checked;
+function themeSwitch(themeChangerSwitched) {
+  themeChangers.forEach(themechanger => {
+    if (themechanger !== themeChangerSwitched) themechanger.checked = !themechanger.checked;
+  });
   themes.forEach(theme => theme.toggleAttribute('disabled'));
   html.toggleAttribute('data-theme-dark');
 }
 
-themeChangers[0].addEventListener('change', function() {
-  themeSwitch(1);
-});
-
-themeChangers[1].addEventListener('change', function() {
-  themeSwitch(0);
-});
+themeChangers.forEach(themeChanger => themeChanger.addEventListener('change', function() {
+  themeSwitch(themeChanger);
+}));
